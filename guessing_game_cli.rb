42dfbcd5e68db85636546147b2a343b68e2c6523
adventurous-class-0665 <rngao97@gmail.com>
2019-09-28 19:01:42 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,16 +1,4 @@
 def run_guessing_game 
 number = rand(5) + 1
+print number
 player_number = 0
-while player_choice != number || player_choice != "exit" || player_choice == number
-puts "Please guess a number"
-player_choice = gets
-if player_choice != number
-  puts "Sorry! The computer guessed #{number}."
-elsif player_choice == number
-  puts "You guessed the correct number!"
-elsif player_choice == "exit"
-  puts "Goodbye!"
-else puts "Invalid choice"
-    end
-  end
-end
