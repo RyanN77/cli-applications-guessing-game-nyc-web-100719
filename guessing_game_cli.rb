0a59498862490923eef8b157f160d14e9a72cd20
adventurous-class-0665 <rngao97@gmail.com>
2019-09-28 18:57:24 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,6 +1,9 @@
 def run_guessing_game 
 number = rand(5) + 1
 player_number = 0
-while player_number != number
-puts "Please type a number"
+puts "Please guess a number"
 player_number = get.chomp
+while 
+if player_number != number
+  puts "Sorry! The computer guessed #{number}."
+elsif 
