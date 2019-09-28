69a310b9e5efe443bc08d59867c0a56263ef0606
adventurous-class-0665 <rngao97@gmail.com>
2019-09-28 19:08:44 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -5,7 +5,7 @@ if input.to_i == number
   print "You guessed the correct number!"
 elsif input.to_i != number
   print "Sorry! The computer guessed #{number}."
-elsif input == "exit"
+elsif input.to_s == "exit"
   print "Goodbye!"
   end
 end
